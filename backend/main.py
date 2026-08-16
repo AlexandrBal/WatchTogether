9ad38b9273398f04f726984d7466d32dd6fcb5b4
AlexandrBal <balamut0527@icloud.com>
2026-08-16 19:29:06 +0300

    Изменены стили, добавлены состояния комнат, вход по id комнаты

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.templating import Jinja2Templates
@@ -24,6 +24,8 @@ app.mount(
 
 relations = {}
 
+roomsInfo = {}
+
 @app.get('/')
 async def index():
     return FileResponse("static/index.html")
@@ -45,6 +47,28 @@ async def get_room(room_id: str, request: Request):
         'room_id': room_id
     })
 
+@app.get('/check_room/{room_id}')
+async def check_room(room_id: str):
+    if room_id in roomsInfo:
+        return JSONResponse(content={'link': f'/rooms/{room_id}', 'error': 0})
+    else:
+        return JSONResponse(content={'link': f'', 'error': 1})
+
+@sio.event
+async def disconnect(sid):
+    if sid not in relations:
+        return
+
+    room = relations[sid]['room']
+
+    if room in roomsInfo:
+        roomsInfo[room]['users'].remove(sid)
+
+        if not roomsInfo[room]['users']:
+            del roomsInfo[room]
+
+    del relations[sid]
+
 @sio.on('join_room')
 async def join_room(sid, data):
     relations[sid] = {
@@ -53,15 +77,45 @@ async def join_room(sid, data):
     }
     await sio.enter_room(sid, data['room_id'])
 
+    if relations[sid]['room'] not in roomsInfo:
+        roomsInfo[data['room_id']] = {
+            'users': [sid],
+            'current_video': '',
+            'current_time': 0,
+            'playing': False
+        }
+    else:
+        roomsInfo[relations[sid]['room']]['users'].append(sid)
+        await sio.emit('join_room', {
+            'curT': roomsInfo[relations[sid]['room']]['current_time'],
+            'curVid': roomsInfo[relations[sid]['room']]['current_video'],
+            'isPlaying': roomsInfo[relations[sid]['room']]['playing']
+            }, to=sid)
+
 @sio.on('load_video')
 async def load_video(sid, data):
-    print(f'На сервере: загрузка видео от {sid}')
+    roomsInfo[relations[sid]['room']]['current_video'] = data['url']
+    roomsInfo[relations[sid]['room']]['current_time'] = 0
+    roomsInfo[relations[sid]['room']]['playing'] = False
     await sio.emit('load_video', data, room=relations[sid]['room'])
 
+@sio.on('set_current_time')
+async def set_current_time(sid, data):
+    if sid not in relations:
+        return
+    room = relations[sid]['room']
+
+    if room not in roomsInfo:
+        return
+
+    roomsInfo[room]['current_time'] = data['currentT']
+
 @sio.on('play_video')
-async def play_video(sid):
-    await sio.emit('play_video', room=relations[sid]['room'], skip_sid=sid)
+async def play_video(sid, data):
+    roomsInfo[relations[sid]['room']]['playing'] = True
+    await sio.emit('play_video', data, room=relations[sid]['room'], skip_sid=sid)
 
 @sio.on('pause_video')
 async def pause_video(sid):
+    roomsInfo[relations[sid]['room']]['playing'] = False
     await sio.emit('pause_video', room=relations[sid]['room'], skip_sid=sid)
