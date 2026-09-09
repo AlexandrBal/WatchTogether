15b7c9e07cd829a85033e1da86255e82517481ee
AlexandrBal <balamut0527@icloud.com>
2026-09-09 21:59:35 +0300

    Небольшое отступление: Имя при входе стало обязательным (для чата in future)

--- static/js/indexScr.js
+++ static/js/indexScr.js
@@ -2,36 +2,42 @@ const roomBtn = document.getElementById("createRoomBtn");
 const newRoomCode = document.getElementById("parag");
 const roomId = document.getElementById('roomID');
 const roomInfo = document.getElementById("roomInfo");
+const usernameInp = document.getElementById('usernameInput');
+const usernameBtn = document.getElementById('createUsernameBtn');
+const spanUsername = document.getElementById('username');
+const entranceBtn = document.getElementById('entranceBtn');
+const entranceID = document.getElementById('entranceID');
+const errorP = document.getElementById('errorP');
+let globalUsername;
 
 roomBtn.addEventListener('click', async () => {
+    if (!globalUsername) {
+        newRoomCode.innerHTML = 'Введите своё имя!';
+        return;
+    }
     const response = await fetch("/create_room", {method: "POST"});
     const data = await response.json();
 
     newRoomCode.innerHTML = `Комната создана: <a href='${data.link}'>Перейти</a>`;
     roomId.innerText = data.room_id;
-
     roomInfo.style.display = "flex";
 })
 
-const usernameInp = document.getElementById('usernameInput');
-const usernameBtn = document.getElementById('createUsernameBtn');
-const spanUsername = document.getElementById('username');
-
 usernameBtn.addEventListener('click', () => {
-
     const username = usernameInp.value
     if (username.trim() === "") return;
+    globalUsername = username;
 
     spanUsername.innerText = username;
 
     localStorage.setItem("username", username);
 })
 
-const entranceBtn = document.getElementById('entranceBtn');
-const entranceID = document.getElementById('entranceID');
-const errorP = document.getElementById('errorP');
-
 entranceBtn.addEventListener('click', async () => {
+    if (!globalUsername) {
+        errorP.innerText = "Введите имя!"
+        return;
+    }
 
     const roomId = entranceID.value.trim();
     if (roomId === "") {
